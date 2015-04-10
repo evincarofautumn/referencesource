0d9232ccd23f247e7000553893d4f755d9ef4499
Jon Purdy <evincarofautumn@gmail.com>
2015-04-10 16:12:53 -0700

    WIP

--- mscorlib/system/io/binaryreader.cs
+++ mscorlib/system/io/binaryreader.cs
@@ -303,8 +303,12 @@ namespace System.IO {
 
                 charsRead = m_decoder.GetChars(m_charBytes, 0, n, m_charBuffer, 0);
 
-                if (currPos == 0 && n == stringLength)
-                    return new String(m_charBuffer, 0, charsRead);
+                if (currPos == 0 && n == stringLength) {
+					String result = new String(m_charBuffer, 0, charsRead);
+					Console.Error.WriteLine("read {0} bytes: \"{1}\"", stringLength, result);
+					Console.Error.Flush();
+                    return result;
+				}
 
                 if (sb == null)
                     sb = StringBuilderCache.Acquire(stringLength); // Actual string length in chars may be smaller.
