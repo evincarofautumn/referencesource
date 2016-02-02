ea67b94e78838fdeed7b377ee890c2d42f860114
Jon Purdy <evincarofautumn@gmail.com>
2016-02-02 14:43:16 -0800

    Fix UnSafeCharBuffer.

--- mscorlib/system/unsafecharbuffer.cs
+++ mscorlib/system/unsafecharbuffer.cs
@@ -19,17 +19,19 @@ namespace System {
     
     unsafe internal struct UnSafeCharBuffer{
         [SecurityCritical]
-        char * m_buffer;
+        byte * m_buffer;
         int m_totalSize;
         int m_length;    
+        bool m_isCompact;
     
         [System.Security.SecurityCritical]  // auto-generated
-        public UnSafeCharBuffer( char *buffer,  int bufferSize) {
+        public UnSafeCharBuffer( byte *buffer,  int bufferSize, bool compact) {
             Contract.Assert( buffer != null, "buffer pointer can't be null."  );
             Contract.Assert( bufferSize >= 0, "buffer size can't be negative."  );        
             m_buffer = buffer;
             m_totalSize = bufferSize;    
             m_length = 0;
+            m_isCompact = compact;
         }
     
         [System.Security.SecuritySafeCritical]  // auto-generated
@@ -43,8 +45,21 @@ namespace System {
             }
             
             fixed( char* pointerToString = stringToAppend ) {        
-                Buffer.Memcpy( (byte*) (m_buffer + m_length), (byte *) pointerToString, stringToAppend.Length * sizeof(char));
-            }    
+#if MONO
+                if (stringToAppend.IsCompact) {
+                    if (m_isCompact) {
+                        Buffer.Memcpy((byte*)(m_buffer + m_length), (byte*)pointerToString, stringToAppend.Length * sizeof(byte));
+                    } else {
+                        for (int i = 0; i < stringToAppend.Length; ++i)
+                            ((char*)m_buffer)[i + m_length] = (char)((byte *)pointerToString) [i];
+                    }
+                } else
+#endif
+                {
+                    Contract.Assert(!m_isCompact, "Cannot fill compact buffer from non-compact strings.");
+                    Buffer.Memcpy( (byte*) (m_buffer + m_length * sizeof(char)), (byte *) pointerToString, stringToAppend.Length * sizeof(char));
+                }
+            }
             
             m_length += stringToAppend.Length;
             Contract.Assert(m_length <= m_totalSize, "Buffer has been overflowed!");
